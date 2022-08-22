3e71c1b63fc74d8ee187937d72215cc4ca5b3fcb
Yogendra Gupta <yogecs@gmail.com>
2022-08-23 02:47:44 +0530

    generate document pdf for agreement

--- application/models/Contracts_model.php
+++ application/models/Contracts_model.php
@@ -100,11 +100,12 @@ class Contracts_model extends App_Model
         }
         $file_name=time().'-'.slug_it($contract->subject) . '.pdf';
         $file_path = CONTRACTS_UPLOADS_FOLDER.$id.'/agreements/'.$file_name;
-        echo $file_path;
+        
         $pdf    = contract_pdf($contract);
         ob_end_flush();
         ob_end_clean();
         $attach = $pdf->Output($file_name, 'F');
+        echo $file_path;
         $file_name_key=slug_it($contract->subject);
         include_once __DIR__."/../controllers/adobe/index.php";
         $doc_array[str_replace('-','_',$file_name_key)]=$file_name;//$attach;
