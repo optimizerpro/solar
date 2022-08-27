d2bd601b5e2a0bc72115b86cdc72577c8803e9fb
Yogendra Gupta <yogecs@gmail.com>
2022-08-27 23:59:23 +0530

    admin signature save in profile

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 * environments.
 *
 */
-define('APP_BASE_URL', 'https://localhost/solar/');
+define('APP_BASE_URL', 'https://hashevo.com/elightsolar/');
 
 /*
 * --------------------------------------------------------------------------
