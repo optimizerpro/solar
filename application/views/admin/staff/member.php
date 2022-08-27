129c86d2d6a4ed4ea0eff06593ee7dacc5b54136
Yogendra Gupta <yogecs@gmail.com>
2022-08-27 23:14:18 +0530

    staff signature

--- application/views/admin/staff/member.php
+++ application/views/admin/staff/member.php
@@ -143,6 +143,10 @@
                      <i class="fa fa-question-circle pull-left" data-toggle="tooltip" data-title="<?php echo _l('staff_email_signature_help'); ?>"></i>
                      <?php $value = (isset($member) ? $member->email_signature : ''); ?>
                      <?php echo render_textarea('email_signature','settings_email_signature',$value, ['data-entities-encode'=>'true']); ?>
+                     <?php
+                        get_template_part('identity_confirmation_form', array('formData' => form_hidden('action', 'sign_contract')));
+                     ?>
+                     <button type="submit" id="accept_action" class="btn btn-success pull-right action-button">Generate your signature</button>
                      <div class="form-group select-placeholder">
                         <label for="direction"><?php echo _l('document_direction'); ?></label>
                         <select class="selectpicker" data-none-selected-text="<?php echo _l('system_default_string'); ?>" data-width="100%" name="direction" id="direction">
