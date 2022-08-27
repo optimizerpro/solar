b8da91572b786f561f5e92f6a60b51635e999c8f
Yogendra Gupta <yogecs@gmail.com>
2022-08-27 22:30:47 +0530

    customer signature in agreement

--- application/libraries/pdf/App_pdf.php
+++ application/libraries/pdf/App_pdf.php
@@ -259,7 +259,7 @@ abstract class App_pdf extends TCPDF
                 $signature = '';
                 if ($this->type() == 'contract') {
                     $imageData = base64_encode(file_get_contents($path));
-                    $src = 'data: '.mime_content_type($path).';base64,'.$imageData;
+                    $src = 'data: image/png;base64,'.$imageData;
                     $signature .= '<br /><img src="'.$src.'" style="width:200px;height:75px;"><br /><span style="font-weight:bold;text-align: left;">';
                     //$signature .= _l('contract_signed_by') . ": {$record->acceptance_firstname} {$record->acceptance_lastname}<br />";
                     $signature .= 'Date : ' . _dt($record->acceptance_date) . '&nbsp;&nbsp;';
